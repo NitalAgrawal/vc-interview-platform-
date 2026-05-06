b384afa794739d79cee26a2701ebfda679189bdf
NitalAgrawal <nitalagrawal5@gmail.com>
2026-05-06 09:40:25 +0530

    Initial commit

---
+++ Backend/src/server.js
@@ -0,0 +1,13 @@
+// const express = require("express")// type :common js
+import express from "express"// type : module
+import{ENV} from "./lib/env.js"
+
+const app= express();
+console.log(ENV.PORT);
+console.log(ENV.DB_URL);
+
+app.get("/health",(req,res) => {
+    res.status(200).json({msg:"api is up and running "});
+});
+
+app.listen(ENV.PORT,()=>console.log("server is running on port :" ,ENV.PORT));
