10063b2aa39dcebeb93bb8c6fcdc75a83cd5918a
NitalAgrawal <nitalagrawal5@gmail.com>
2026-05-14 13:51:01 +0530

    new changes

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -3,10 +3,19 @@ import express from "express"// type : module
 import{ENV} from "./lib/env.js"
 import path from "path";
 import { connectDB } from "./lib/db.js";
+import{serve} from "inngest/express";
+import {inngest} from "./lib/inngest.js"
 
 const app= express();
 
 const __dirname=path.resolve()
+
+// middleware
+app.use(express.json())
+// credentials true means => server allows a browser to include cookies on request
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))
+
+app.use("/api/inngest", serve({ client: inngest,functions}))
 
 app.get("/health",(req,res) => {
     res.status(200).json({msg:"api is up and running "});
